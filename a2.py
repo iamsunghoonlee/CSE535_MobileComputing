4577ba862084b523af5f04e1225802222c17e286
Sun Lee <iamsunghoonlee@gmail.com>
2025-09-13 21:38:36 -0400

    Markov Chain assignment

---
+++ a2.py
@@ -0,0 +1,14 @@
+class A2:
+    def __init__(self):
+        return
+    
+    def generate_public_key_vector(self, matrix_A, secret_key, error_vec, modulus_p):
+        
+        return vector_b
+    
+    def encrypt(self, public_key, binary_vector, message, modulus_p):
+        
+        return encryption_tuple
+    
+    def decrypt(self, private_key, cipher_text, modulus_p):
+        return decrypted_message
